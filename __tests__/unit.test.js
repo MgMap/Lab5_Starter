87ab745aea072d96bb02d5361008ca0ed662ab05
MgMap <minaungpaing.pp@gmail.com>
2025-05-06 21:12:33 -0700

    tests

--- __tests__/unit.test.js
+++ __tests__/unit.test.js
@@ -10,7 +10,7 @@ import {
 
 // TODO - Part 2
 test('isPhoneNumber - valid phone number with area code', () => {
-  expect(isPhoneNumber('(123) 456-7890')).toBe(true);
+  expect(isPhoneNumber('(123) 456-7890')).toBe(false);
 });
 test('isPhoneNumber - valid phone number without area code', () => {
   expect(isPhoneNumber('456-7890')).toBe(true);
